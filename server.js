2613b8c5d64ed1a23f66a3c86ba1af7e7adaf107
dip_sarker <sarkerdip005@gmail.com>
2023-03-08 16:34:08 +0600

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors=require("cors")
 
 
 //middleware.......
-app.use(cors())
+//app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
